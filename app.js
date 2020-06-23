8d159d7369a622b38e2829a9bfd853dfa06102f4
hozerpffp <hozerpffp2009@gmail.com>
2020-06-22 19:40:39 -0500

    added css styling for manager, engineer, intern

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const inquirer = require("inquirer");
-// const path = require("path");
 const fs = require("fs");
-// const render = require("./lib/htmlRenderer");
-// const OUTPUT_DIR = path.resolve(__dirname, "output");
-// const outputPath = path.join(OUTPUT_DIR, "team.html");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-
+// const path = require("path");
+// const render = require("./lib/htmlRenderer");
+// const OUTPUT_DIR = path.resolve(__dirname, "output");
+// const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
@@ -54,8 +53,8 @@ async function begin() {
                     message: "what is the employees job title?",
                     name: "title",
                     choices: [
-                        "Engineer",
                         "Manager",
+                        "Engineer",
                         "Intern"
                     ]
                 }
